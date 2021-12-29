a4bb3ce5c25a8c25e3244299feb4a46fc28996a9
Paulo Henrique Dias <34211685+PauloHenriqueDias@users.noreply.github.com>
2021-12-29 19:34:35 -0300

    Add files via upload

---
+++ cadastro/cadastro-delete/cadastro-delete.component.ts
@@ -0,0 +1,45 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { CadastroService } from './../cadastro.service';
+import { Component, OnInit } from '@angular/core';
+import { Cadastro } from '../cadastro.model';
+
+
+@Component({
+  selector: 'app-cadastro-delete',
+  templateUrl: './cadastro-delete.component.html',
+  styleUrls: ['./cadastro-delete.component.css']
+})
+export class CadastroDeleteComponent implements OnInit {
+
+  cadastro: Cadastro;
+
+  constructor(private cadastroService: CadastroService,
+              private router: Router,
+              private route: ActivatedRoute
+    ) {}
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.cadastroService.readById(id).subscribe(cadastro =>{
+      this.cadastro = cadastro;
+    })
+  }
+
+  deletarCadastro(): void{
+    this.cadastroService.deleteCadastro(this.cadastro.id).subscribe(() => {
+      this.cadastroService.showMessege('Cadastro Deletado')
+      this.router.navigate(['/cadastro/tabela'])
+    })
+  }
+
+  cancelarCadastro(): void{
+    this.router.navigate(['/cadastro'])
+
+  }
+
+  tabelasCadastro(): void{
+    this.router.navigate(['/cadastro/tabela'])
+
+  }
+
+}
